fdd174444dc64cbdc106c491c6c0f5e54cd02c5d
LinWenfen <2071198926@qq.com>
2023-10-05 23:09:17 +0800

    remove FileManage.cpp again

--- DrillingForLearnGit/FileManage.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main() {
-	cout << "Git is a distributed version control system." << endl;
-	cout << "Git is free software distributed under the GPL." << endl;
-	return 0;
-}
-
