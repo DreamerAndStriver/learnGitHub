ce629a9884b6a0980b98290c9292dc821a34f42d
LinWenfen <2071198926@qq.com>
2023-10-07 11:28:22 +0800

    add 2 files agian

---
+++ DrillingForLearnGit/branchManage.cpp
@@ -0,0 +1,5 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "this is a cpp file for learning branch management." << endl;
+}
---
+++ DrillingForLearnGit/helloSoftware.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+int main() {
+	cout << "hello Software!" << endl;
+	return 0;
+}
